9e75fd87c2799f67ab9cf6b7500d9f597b8a178b
Macbook <danyzavala94@gmail.com>
2019-05-19 14:40:57 -0500

    add vuex to app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {store} from "./store/store"
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/scss/main.scss'
+
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
